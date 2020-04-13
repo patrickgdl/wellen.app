5c6c668019cf7e6ad76f702f02e9768d8ee725d6
patrickgdl <patrickgdlima@gmail.com>
2020-04-13 17:02:04 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,236 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+import { CanvasAudioStyle } from './models/canvas-audio-style.interface';
+import { HttpClient } from '@angular/common/http';
+
+const FFT_SIZE = 512;
+const TYPE = {
+  lounge: 'renderLounge'
+};
+
+@Component({
+  selector: 'wel-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit, OnInit {
+  audioEl: HTMLAudioElement;
+  canvasEl: HTMLCanvasElement;
+  audioCtx: AudioContext;
+  canvasCtx: CanvasRenderingContext2D;
+  analyser: AnalyserNode;
+  frequencyData: Uint8Array;
+  sourceNode: AudioBufferSourceNode;
+  audioSrc: string;
+  gradient: CanvasGradient;
+  authorAttr: string; // audio.getAttribute('data-author') || '';
+  titleAttr: string; // audio.getAttribute('data-title') || '';
+
+  canvasStyle: CanvasAudioStyle;
+
+  interval = null;
+  isPlaying = false;
+  duration = 0;
+  minutes = '00';
+  seconds = '00';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.audioEl = document.getElementById('myAudio') as HTMLAudioElement;
+    this.canvasEl = document.getElementById('myCanvas') as HTMLCanvasElement;
+    this.canvasCtx = this.canvasEl.getContext('2d');
+
+    this.authorAttr = this.audioEl.getAttribute('data-author');
+    this.titleAttr = this.audioEl.getAttribute('data-title');
+
+    this.canvasStyle = {
+      style: 'lounge',
+      barWidth: 2,
+      barHeight: 2,
+      barSpacing: 7,
+      barColor: '#cafdff',
+      shadowBlur: 20,
+      shadowColor: '#ffffff',
+      font: ['12px', 'Helvetica']
+    };
+
+    this._createVisualizer();
+  }
+
+  setContext() {
+    try {
+      this.audioCtx = new window.AudioContext();
+    } catch (e) {
+      console.log('Web Audio API is not supported.', e);
+    }
+  }
+
+  setAnalyser() {
+    this.analyser = this.audioCtx.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.6;
+    this.analyser.fftSize = FFT_SIZE;
+  }
+
+  setFrequencyData() {
+    this.frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
+  }
+
+  setBufferSourceNode() {
+    this.sourceNode = this.audioCtx.createBufferSource();
+    this.sourceNode.loop = true; // loop property
+    this.sourceNode.connect(this.analyser);
+    this.sourceNode.connect(this.audioCtx.destination);
+
+    this.sourceNode.onended = function() {
+      clearInterval(this.interval);
+      this.sourceNode.disconnect();
+      this.resetTimer();
+      this.isPlaying = false;
+      this.sourceNode = this.ctx.createBufferSource();
+    }.bind(this);
+  }
+
+  setMediaSource() {
+    this.audioSrc = this.audioEl.getAttribute('src');
+  }
+
+  setCanvasStyles() {
+    this.gradient = this.canvasCtx.createLinearGradient(0, 0, 0, 300);
+    this.gradient.addColorStop(1, this.canvasStyle.barColor);
+
+    this.canvasCtx.fillStyle = this.gradient;
+    this.canvasCtx.shadowBlur = this.canvasStyle.shadowBlur;
+    this.canvasCtx.shadowColor = this.canvasStyle.shadowColor;
+    this.canvasCtx.font = this.canvasStyle.font.join(' ');
+    this.canvasCtx.textAlign = 'center';
+  }
+
+  onClick() {
+    if (!this.isPlaying) {
+      return this.audioCtx.state === 'suspended' ? this.playSound() : this.loadSound();
+    } else {
+      return this.pauseSound();
+    }
+  }
+
+  loadSound() {
+    this.canvasCtx.fillText('Loading...', this.canvasEl.width / 2 + 10, this.canvasEl.height / 2);
+
+    this.http.get<ArrayBuffer>(this.audioSrc).subscribe(response => {
+      this.audioCtx.decodeAudioData(response, this.playSound(response), this.onError(response));
+    });
+  }
+
+  playSound(buffer?: AudioBuffer) {
+    this.isPlaying = true;
+
+    if (this.audioCtx.state === 'suspended') {
+      return this.audioCtx.resume();
+    }
+
+    this.sourceNode.buffer = buffer;
+    this.sourceNode.start(0);
+    this.resetTimer();
+    this.startTimer();
+    this.renderFrame();
+  }
+
+  pauseSound() {
+    this.audioCtx.suspend();
+    this.isPlaying = false;
+  }
+
+  startTimer() {
+    this.interval = setInterval(function() {
+      if (this.isPlaying) {
+        const now = new Date(this.duration);
+        const min = now.getHours();
+        const sec = now.getMinutes();
+        this.minutes = min < 10 ? '0' + min : min;
+        this.seconds = sec < 10 ? '0' + sec : sec;
+        this.duration = now.setMinutes(sec + 1);
+      }
+    }, 1000);
+  }
+
+  resetTimer() {
+    const time = new Date(0, 0);
+    this.duration = time.getTime();
+  }
+
+  onError(e) {
+    console.log('Error decoding audio file. -- ', e);
+  }
+
+  renderFrame() {
+    requestAnimationFrame(this.renderFrame.bind(this));
+    this.analyser.getByteFrequencyData(this.frequencyData);
+
+    this.canvasCtx.clearRect(0, 0, this.canvasEl.width, this.canvasEl.height);
+
+    this.renderTime();
+    this.renderText();
+    this.renderByStyleType();
+  }
+
+  renderText() {
+    const cx = this.canvasEl.width / 2;
+    const cy = this.canvasEl.height / 2;
+    const correction = 10;
+
+    this.canvasCtx.textBaseline = 'top';
+    this.canvasCtx.fillText('by ' + this.authorAttr, cx + correction, cy);
+    this.canvasCtx.font = parseInt(this.canvasStyle.font[0], 10) + 8 + 'px ' + this.canvasStyle.font[1];
+    this.canvasCtx.textBaseline = 'bottom';
+    this.canvasCtx.fillText(this.titleAttr, cx + correction, cy);
+    this.canvasCtx.font = this.canvasStyle.font.join(' ');
+  }
+
+  renderTime() {
+    const time = this.minutes + ':' + this.seconds;
+    this.canvasCtx.fillText(time, this.canvasEl.width / 2 + 10, this.canvasEl.height / 2 + 40);
+  }
+
+  renderByStyleType() {
+    return this[TYPE[this.canvasStyle.style]]();
+  }
+
+  renderLounge() {
+    const cx = this.canvasEl.width / 2;
+    const cy = this.canvasEl.height / 2;
+    const radius = 140;
+    const maxBarNum = Math.floor((radius * 2 * Math.PI) / (this.canvasStyle.barWidth + this.canvasStyle.barSpacing));
+    const slicedPercent = Math.floor((maxBarNum * 25) / 100);
+    const barNum = maxBarNum - slicedPercent;
+    const freqJump = Math.floor(this.frequencyData.length / maxBarNum);
+
+    for (let i = 0; i < barNum; i++) {
+      const amplitude = this.frequencyData[i * freqJump];
+      const alfa = (i * 2 * Math.PI) / maxBarNum;
+      const beta = ((3 * 45 - this.canvasStyle.barWidth) * Math.PI) / 180;
+      const x = 0;
+      const y = radius - (amplitude / 12 - this.canvasStyle.barHeight);
+      const w = this.canvasStyle.barWidth;
+      const h = amplitude / 6 + this.canvasStyle.barHeight;
+
+      this.canvasCtx.save();
+      this.canvasCtx.translate(cx + this.canvasStyle.barSpacing, cy + this.canvasStyle.barSpacing);
+      this.canvasCtx.rotate(alfa - beta);
+      this.canvasCtx.fillRect(x, y, w, h);
+      this.canvasCtx.restore();
+    }
+  }
+
+  private _createVisualizer() {
+    this.setContext();
+    this.setAnalyser();
+    this.setFrequencyData();
+    this.setBufferSourceNode();
+    this.setMediaSource();
+    this.setCanvasStyles();
+  }
+}
---
+++ src/app/models/canvas-audio-style.interface.ts
@@ -0,0 +1,10 @@
+export interface CanvasAudioStyle {
+    style: string; // cfg.style || 'lounge';
+    barWidth: number; // cfg.barWidth || 2;
+    barHeight: number; // cfg.barHeight || 2;
+    barSpacing: number; // cfg.barSpacing || 5;
+    barColor: string; // cfg.barColor || '#ffffff';
+    shadowBlur: number; // cfg.shadowBlur || 10;
+    shadowColor: string; // cfg.shadowColor || '#ffffff';
+    font: string[]; // cfg.font || ['12px', 'Helvetica'];
+}
